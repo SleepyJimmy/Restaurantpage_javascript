a660dc3fd548f62593aa8d65e6047334fea1557e
SleepyJimmy <lijunfang97@gmail.com>
2024-11-28 19:20:50 +0000

    Added fonts and implemented logic when clicking the menu tab

--- src/tab1.js
+++ src/tab1.js
@@ -1,3 +1,64 @@
+function createElement(tag, className = "", innerHTML = "") {
+    const element = document.createElement(tag);
+    if (className) element.className = className;
+    if (innerHTML) element.innerHTML = innerHTML;
+    return element;
+}
+
 export function loadTab1() {
-    console.log("tab1");
+    // clear current content in the div
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+    
+    // Create and add title container
+    const titleContainer = createElement("div", "title-container");
+    const title = createElement("div", "title", "Beary's Breakfast Bar");
+    titleContainer.appendChild(title);
+    
+    // create and add about container
+    const aboutContainer = createElement("div", "about-container");
+    const about = createElement("div", "about");
+    const about1 = createElement("p", "", "Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.")
+    const about2 = createElement("p", "", "Goldilocks");
+    about.appendChild(about1);
+    about.appendChild(about2);
+    aboutContainer.appendChild(about);    
+
+    // create and add hours container
+    const hoursContainer = createElement("div", "hours-container");
+    const hours = createElement("div", "hours");
+    const hours1 = createElement("p", "", "Hours")
+    const hoursList = createElement("ul", "hours-items");
+    const hoursData = [
+        { day: "Sunday", hours: "8am - 8pm" },
+        { day: "Monday", hours: "6am - 6pm" },
+        { day: "Tuesday", hours: "6am - 6pm" },
+        { day: "Wednesday", hours: "6am - 6pm" },
+        { day: "Thursday", hours: "6am - 10pm" },
+        { day: "Friday", hours: "6am - 10pm" },
+        { day: "Saturday", hours: "8am - 10pm" },
+    ];
+    hoursData.forEach(({day, hours}) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${day}: ${hours}`;
+        hoursList.appendChild(listItem);
+    });
+    hours.appendChild(hours1);
+    hours.appendChild(hoursList);
+    hoursContainer.appendChild(hours);
+
+    // create and add loation container
+    const locationContainer = createElement("div", "location-container");
+    const location = createElement("div", "location");
+    const location1 = createElement("p", "", "Location");
+    const location2 = createElement("p", "", "123 Forest Drive, Forestville, Maine");
+    location.appendChild(location1);
+    location.appendChild(location2);
+    locationContainer.appendChild(location);
+
+    // append created containers into the main content div
+    content.appendChild(titleContainer);
+    content.appendChild(aboutContainer);
+    content.appendChild(hoursContainer);
+    content.appendChild(locationContainer);
 }
--- src/tab2.js
+++ src/tab2.js
@@ -1,3 +1,76 @@
+function createElement(tag, className = "", innerHTML = "") {
+    const element = document.createElement(tag);
+    if (className) element.className = className;
+    if (innerHTML) element.innerHTML = innerHTML;
+    return element;
+}
+    
+
+function createMenuItem(itemName, descrip, price) {
+    const elementContainer = createElement("div", "item-container", "");
+    const element = document.createElement("div");
+    element.className = "item";
+    const item = createElement("p", "", itemName);
+    const des = createElement("p", "", descrip);
+    const pri = createElement("p", "", price);
+    element.appendChild(item);
+    element.appendChild(des);
+    element.appendChild(pri);
+    elementContainer.appendChild(element);
+    return elementContainer
+}
+
 export function loadTab2() {
-    console.log("tab2");
+    // clear current content in the div
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+    
+    // Create and add title container
+    const titleContainer = createElement("div", "title-container");
+    const title = createElement("div", "title", "Menu");
+    titleContainer.appendChild(title);
+
+    const beveragesSubheader = createElement("div", "subheader-container", "");
+    const beverages = createElement("div", "subheader", "Beverages");
+    beveragesSubheader.appendChild(beverages);
+    const bev1 = createMenuItem("Honey Tea", "A warm, sweet tea made with the highest quality honey and a bit of lemon to start your day off right!", "$2");
+    const bev2 = createMenuItem("Beary Tea", "A comforting, almost filling, tea that is infused with the flavors of several kinds of berries. Best served cold, but can be served hot on request.", "$3");
+
+    const sidesSubheader = createElement("div", "subheader-container", "");
+    const sides = createElement("div", "subheader", "Sides");
+    sidesSubheader.appendChild(sides);
+    const side1 = createMenuItem("Toast and Jam", "A slice of toast, your choice of bread, and our homemade blackberry or raspberry jam.", "$1");
+    const side2 = createMenuItem("Fresh Fruit", "A small bowl of fresh fruit, whatever we find at the market for the day.", "$3");
+   
+   
+    const mainSubheader = createElement("div", "subheader-container", "");
+    const main = createElement("div", "subheader", "Main Dishes");
+    mainSubheader.appendChild(main);
+    const main1 = createMenuItem("Pancakes", "A stack of homemade buttermilk pancakes, served with our locally sourced maple syrup.", "$4");
+    const main2 = createMenuItem("French Toast", "Two slices of the best french toast you will ever eat, served with our locally sourced maple syrup.", "$5");
+    const main3 = createMenuItem("Beary Veggie Sandwich", "Do you like vegetables? Then this is the sandwich for you! Stuffed full of a variety of fresh produce, it will fill you up.", "$8");
+    const main4 = createMenuItem("BLT", "Interested in the Beary Veggie Sandwich but also love bacon? Say no more.", "$6");
+    const main5 = createMenuItem("Bagel and Lax", "Our house specialty, you can't go wrong with a hearty bagel topped with sustainably harvested salmon.", "$8");
+    const main6 = createMenuItem("Honeycomb", "Are you a bear like us? Then you will love our honeycomb. And, yes humans, it is just a piece of honeycomb, not the popular breakfast cereal.", "$6");
+    const main7 = createMenuItem("Beary Bowl", "Get a big ole bowl of our berries! Side of honey is $1 extra.", "$7");
+    const main8 = createMenuItem("The Beary Best Porridge", "Made by Baby Bear himself, this porridge is guarenteed to be just right, or your money back.", "$5");
+    
+
+    content.appendChild(titleContainer);
+    content.appendChild(beveragesSubheader);
+    content.appendChild(bev1);
+    content.appendChild(bev2);
+    content.appendChild(sidesSubheader);
+    content.appendChild(side1);
+    content.appendChild(side2);
+    
+    content.appendChild(mainSubheader);
+    content.appendChild(main1);
+    content.appendChild(main2);
+    content.appendChild(main3);
+    content.appendChild(main4);
+    content.appendChild(main5);
+    content.appendChild(main6);
+    content.appendChild(main7);
+    content.appendChild(main8);
 }

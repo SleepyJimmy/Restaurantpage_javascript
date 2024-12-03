e891a40f1c49045b3d40a4d1780cb64af823741c
SleepyJimmy <lijunfang97@gmail.com>
2024-12-03 20:06:40 +0000

    Implemented page to load first tab initially

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ let tab1 = document.getElementById("tab1");
 let tab2 = document.getElementById("tab2");
 let tab3 = document.getElementById("tab3");
 
+loadTab1();
+
 tab1.addEventListener("click", () => {
     loadTab1();
 });
